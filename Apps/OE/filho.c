4da3b7847bb892816b650ad9d8f1cfe71f55d0f8
Adriano Marques Garcia <adriano1mg@gmail.com>
2019-06-25 14:38:12 -0300

    update filho.c

--- Apps/OE/filho.c
+++ Apps/OE/filho.c
@@ -3,23 +3,19 @@
 #include <stdlib.h>
 #include <time.h>
 
-#ifdef PAPI
-	#include <papi.h>
-#endif
-
-MPI_Comm interCommPai;
+MPI_Comm interCommPai = 0;
 MPI_Request request[3];
-long int N = 0, *vetor = NULL, *vetIndice = NULL, size = 0, indice = 0;
+int N = 0, *vetor = NULL, *vetIndice = NULL, size = 0, indice = 0;
 
 void leEntrada(){
-	long int i;
+	int i;
 	for(i=0;i<N;i++){
 		vetor[i] = N-i;
 	}
 }
 
 void sortPar(int rank){
-	long int aux, i;
+	int aux, i;
 	for(i=vetIndice[rank]; i<vetIndice[(rank+size)]; i = i+2){
 		if(vetor[i] > vetor[i+1]){
 			aux = vetor[i];
@@ -31,7 +27,7 @@ void sortPar(int rank){
 }
 
 void sortImpar(int rank){
-	long int aux, i;
+	int aux, i;
 	for(i=vetIndice[(size*2)+rank]; i<vetIndice[(size*3)+rank]; i = i+2){
 		if(vetor[i] > vetor[i+1]){
 			aux = vetor[i];
@@ -42,10 +38,10 @@ void sortImpar(int rank){
 }
 
 void bubbleSort(int rank){
-	long int i;
-	long int aux = N/size;
+	int i;
+	int aux = N/size;
 	int id = 99;
-	
+
 	if(size == 2){
 		for(i=0;i<(N/2)+1;i++){	
 			MPI_Irecv(&vetor[aux-1], 1, MPI_INT, 0, id, interCommPai, &request[0]);
@@ -57,14 +53,12 @@ void bubbleSort(int rank){
 		MPI_Send(&vetor[aux-1], (N-(aux-1))+1, MPI_INT, 0, id, interCommPai);	
 	}
 	else{
-
 		for(i=0;i<(N/2)+1;i++){
 			if(rank == 0){
 				MPI_Irecv(&vetor[((rank+1)*aux)-1], 1, MPI_INT, 0, id, interCommPai, &request[0]);
-				//printf("filho %d Irecv %d\n", rank, id);
-			}else{
+			}
+			else{
 				MPI_Irecv(&vetor[((rank+1)*aux)-1], 1, MPI_INT, rank-1, id+rank, MPI_COMM_WORLD, &request[0]);
-			//	printf("filho %d Irecv %d\n", rank, id+rank);
 			}
 
 			sortPar(rank+1);
@@ -74,9 +68,7 @@ void bubbleSort(int rank){
 			}else{
 				MPI_Send(&vetor[((rank+2)*aux)-1], 1, MPI_INT, rank+1, id+rank+1, MPI_COMM_WORLD);
 				MPI_Wait(&request[0], MPI_STATUS_IGNORE);
-			//	printf("filho %d Send %d\n", rank, id+rank+1);
 				MPI_Irecv(&vetor[((rank+2)*aux)-1], 1, MPI_INT, rank+1, id+rank+1, MPI_COMM_WORLD, &request[1]);
-			//	printf("filho %d Irecv %d\n", rank, id+rank+1);
 			}
 
 			sortImpar(rank+1);
@@ -84,71 +76,47 @@ void bubbleSort(int rank){
 			if(rank == 0){
 				MPI_Send(&vetor[((rank+1)*aux)-1], 1, MPI_INT, 0, id, interCommPai);
 				MPI_Wait(&request[1], MPI_STATUS_IGNORE);
-			//	printf("filho %d Send %d\n", rank, id);
 			}else if(rank == size-2){
 				MPI_Send(&vetor[((rank+1)*aux)-1], 1, MPI_INT, rank-1, id+rank, MPI_COMM_WORLD);
-			//	printf("filho %d Send %d\n", rank, id+rank);
 			}else{
 				MPI_Send(&vetor[((rank+1)*aux)-1], 1, MPI_INT, rank-1, id+rank, MPI_COMM_WORLD);
 				MPI_Wait(&request[1], MPI_STATUS_IGNORE);
-			//	printf("filho %d Send %d\n", rank, id+rank);
 			}
 		}	
 		if(rank == size-2){
 			MPI_Send(&vetor[((rank+1)*aux)-1], aux+1, MPI_INT, 0, id+(rank*100), interCommPai);
-			printf("filho %d\tvector[%ld]\taux: %ld\t Send: %d\n", rank, ((rank+1)*aux)-1, aux+1, id+(rank*100));
 		}else{
 			MPI_Send(&vetor[((rank+1)*aux)-1], aux, MPI_INT, 0, id+(rank*100), interCommPai);
-
-			printf("filho %d\tvector[%ld]\taux: %ld\t Send: %d\n", rank, ((rank+1)*aux)-1, aux, id+(rank*100));
 		}
 	}
 }
 
 int main(int argc, char **argv){
 
-	argv+=1;
-	#ifdef PAPI
-                char *papi_hex_code = argv[3];
-                int PAPI_CODE = (int)strtol(papi_hex_code, NULL, 16); // get the code of a PAPI event
-                int EventSet = PAPI_NULL;
-                long long event_result;
-                PAPI_library_init(PAPI_VER_CURRENT);
-                PAPI_create_eventset(&EventSet);
-                PAPI_add_event(EventSet, PAPI_CODE);
-                PAPI_start(EventSet);
-                PAPI_reset(EventSet);
-        #endif
+		argv+=1;
 
-	MPI_Init(&argc, &argv);
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_get_parent(&interCommPai);
+		MPI_Init(&argc, &argv);
+		int rank;
+		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+		MPI_Comm_get_parent(&interCommPai);
 	
 
-	//int i;
-	//for(i=0;i<7;i++)
-	//	printf("arg %d = %s\n", i, argv[i]);
+		//int i;
+		//for(i=0;i<7;i++)
+		//	printf("arg %d = %s\n", i, argv[i]);
 
-	N = atoi(argv[2]);
+		N = atoi(argv[2]);
 		
-	int numFilhos = atoi(argv[4]);
-	size = numFilhos+1;
-	vetIndice = malloc(sizeof(long int)*size*4);
-	vetor = malloc(sizeof(long int)*N);
-	leEntrada();
-	MPI_Recv(&vetIndice[0], size*4, MPI_INT, 0, 99, interCommPai, MPI_STATUS_IGNORE);
+		int numFilhos = atoi(argv[4]);
+		size = numFilhos+1;
+		vetIndice = malloc(sizeof(int)*size*4);
+		vetor = malloc(sizeof(int)*N);
+		leEntrada();
+		MPI_Recv(&vetIndice[0], size*4, MPI_INT, 0, 99, interCommPai, MPI_STATUS_IGNORE);
 		
-	//printf("RQRQRQRQRQRQR\n");
-	bubbleSort(rank);
+		bubbleSort(rank);
 
-	MPI_Finalize();
+		MPI_Finalize();
 
-        #ifdef PAPI
-                PAPI_read(EventSet, &event_result);
-                printf("FILHO\tPAPI EVENT\t%s\tVALUE\t%llu\n", papi_hex_code, event_result);
-                PAPI_stop(EventSet, NULL);
-        #endif
-	return 0;
+		return 0;
 }
-
