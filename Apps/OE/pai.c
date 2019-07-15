a5d724d4f38c20dd645ad8cbbe50f50886bcf755
Adriano Marques Garcia <adriano1mg@gmail.com>
2019-07-15 18:35:45 -0300

    Update pai.c

--- Apps/OE/pai.c
+++ Apps/OE/pai.c
@@ -1,34 +1,27 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
+#include <sys/time.h>
 
-MPI_Comm interCommPai = 0;
-MPI_Request request[3];
-int N = 0, *vetor = NULL, *vetIndice = NULL, size = 0, indice = 0;
-
-void leEntrada(){
-	int i;
-	for(i=0;i<N;i++){
-		vetor[i] = N-i;
-	}
-}
+int N = 0, *vetIndices, *vetor, size, indice;
+MPI_Comm interComm;
+MPI_Request request[128];
 
 void sortPar(int rank){
 	int aux, i;
-	for(i=vetIndice[rank]; i<vetIndice[(rank+size)]; i = i+2){
+	for(i=vetIndices[(rank*size)+rank]; i<vetIndices[size+rank]; i = i+2){
 		if(vetor[i] > vetor[i+1]){
 			aux = vetor[i];
 			vetor[i] = vetor[i+1];
 			vetor[i+1] = aux;
 		}
 	}
-
 }
 
 void sortImpar(int rank){
 	int aux, i;
-	for(i=vetIndice[(size*2)+rank]; i<vetIndice[(size*3)+rank]; i = i+2){
+	for(i=vetIndices[(size*2)+rank]; i<vetIndices[(size*3)+rank]; i = i+2){
 		if(vetor[i] > vetor[i+1]){
 			aux = vetor[i];
 			vetor[i] = vetor[i+1];
@@ -37,86 +30,112 @@ void sortImpar(int rank){
 	}
 }
 
-void bubbleSort(int rank){
+void defineChunk(int size){
 	int i;
 	int aux = N/size;
+	
+	for(i=0; i<size; i++){
+		if(i==0)
+			vetIndices[i] = 0;
+		else
+			vetIndices[i] = i*aux;
+	}
+	for(i=0; i<size; i++)
+		vetIndices[i+size] = ((i+1)*aux)-1;
+	
+	for(i=0; i<size; i++){
+		if(i==0)
+			vetIndices[i+(2*size)] = 1;
+		else
+			vetIndices[i+(2*size)] = (i*aux)-1;
+	}
+	for(i=0; i<size; i++){
+		if (i==size-1)
+			vetIndices[i+(3*size)] = N-1;
+		else
+			vetIndices[i+(3*size)] = ((i+1)*aux)-2;
+	}
+}
+
+void bubbleSort(){
+	int i, source;
+	int aux = N/size;
 	int id = 99;
-
-	if(size == 2){
-		for(i=0;i<(N/2)+1;i++){	
-			MPI_Irecv(&vetor[aux-1], 1, MPI_INT, 0, id, interCommPai, &request[0]);
-			sortPar(1);
-			MPI_Wait(&request[0], MPI_STATUS_IGNORE);
-			sortImpar(1);
-			MPI_Send(&vetor[aux-1], 1, MPI_INT, 0, id, interCommPai);
-		}
-		MPI_Send(&vetor[aux-1], (N-(aux-1))+1, MPI_INT, 0, id, interCommPai);	
-	}
-	else{
-		for(i=0;i<(N/2)+1;i++){
-			if(rank == 0){
-				MPI_Irecv(&vetor[((rank+1)*aux)-1], 1, MPI_INT, 0, id, interCommPai, &request[0]);
-			}
-			else{
-				MPI_Irecv(&vetor[((rank+1)*aux)-1], 1, MPI_INT, rank-1, id+rank, MPI_COMM_WORLD, &request[0]);
-			}
-
-			sortPar(rank+1);
-				
-			if(rank == size-2){
-				MPI_Wait(&request[0], MPI_STATUS_IGNORE);
-			}else{
-				MPI_Send(&vetor[((rank+2)*aux)-1], 1, MPI_INT, rank+1, id+rank+1, MPI_COMM_WORLD);
-				MPI_Wait(&request[0], MPI_STATUS_IGNORE);
-				MPI_Irecv(&vetor[((rank+2)*aux)-1], 1, MPI_INT, rank+1, id+rank+1, MPI_COMM_WORLD, &request[1]);
-			}
-
-			sortImpar(rank+1);
 			
-			if(rank == 0){
-				MPI_Send(&vetor[((rank+1)*aux)-1], 1, MPI_INT, 0, id, interCommPai);
-				MPI_Wait(&request[1], MPI_STATUS_IGNORE);
-			}else if(rank == size-2){
-				MPI_Send(&vetor[((rank+1)*aux)-1], 1, MPI_INT, rank-1, id+rank, MPI_COMM_WORLD);
-			}else{
-				MPI_Send(&vetor[((rank+1)*aux)-1], 1, MPI_INT, rank-1, id+rank, MPI_COMM_WORLD);
-				MPI_Wait(&request[1], MPI_STATUS_IGNORE);
-			}
-		}	
-		if(rank == size-2){
-			MPI_Send(&vetor[((rank+1)*aux)-1], aux+1, MPI_INT, 0, id+(rank*100), interCommPai);
-		}else{
-			MPI_Send(&vetor[((rank+1)*aux)-1], aux, MPI_INT, 0, id+(rank*100), interCommPai);
-		}
+	for(i=0;i<(N/2)+1;i++){
+		sortPar(0);
+		MPI_Send(&vetor[aux-1], 1, MPI_INT, 0, id, interComm);
+		MPI_Irecv(&vetor[aux-1], 1, MPI_INT, 0, id, interComm, &request[0]);
+		sortImpar(0);
+		MPI_Wait(&request[0], MPI_STATUS_IGNORE);
 	}
+	if(size==2){
+		MPI_Recv(&vetor[aux-1], (N-(aux-1))+1, MPI_INT, 0, id, interComm, MPI_STATUS_IGNORE);
+	}else{
+		for(i=0; i<size-2; i++)
+			MPI_Irecv(&vetor[((i+1)*aux)-1], aux, MPI_INT, i, id+(i*100), interComm, &request[i]);
+			
+		MPI_Irecv(&vetor[((size-1)*aux)-1], aux+1, MPI_INT, size-2, id+((size-2)*100), interComm, &request[size-2]);
+		
+		for(i=0;i<size-1;i++)
+			MPI_Waitany(size-1, request, &source, MPI_STATUS_IGNORE);
+	}
+}
+
+
+
+void leEntrada(){
+	int i;
+	for(i=0;i<N;i++)
+		vetor[i] = N-i;
 }
 
 int main(int argc, char **argv){
 
-		argv+=1;
-
-		MPI_Init(&argc, &argv);
-		int rank;
-		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-		MPI_Comm_get_parent(&interCommPai);
+	#ifdef ELAPSEDTIME
+		struct timeval start, end;
+		gettimeofday(&start, NULL);
+	#endif
 	
+	MPI_Init(&argc, &argv);
+  	MPI_Info localInfo;
+	MPI_Info_create(&localInfo);
+	long long valor;
+	
+	int numFilhos = atoi(argv[1]);
+	size = numFilhos+1;
+	N = atoi(argv[2]);       
+                	
+	char *bin = realpath(argv[argc-1], NULL);
+	
+	vetor = malloc(sizeof(int)*N);
+	vetIndices = malloc(sizeof(int)*(size)*4);
+	defineChunk(numFilhos+1);
 
-		//int i;
-		//for(i=0;i<7;i++)
-		//	printf("arg %d = %s\n", i, argv[i]);
-
-		N = atoi(argv[2]);
+	leEntrada();
+	int err[1], i;
 		
-		int numFilhos = atoi(argv[1]);
-		size = numFilhos+1;
-		vetIndice = malloc(sizeof(int)*size*4);
-		vetor = malloc(sizeof(int)*N);
-		leEntrada();
-		MPI_Recv(&vetIndice[0], size*4, MPI_INT, 0, 99, interCommPai, MPI_STATUS_IGNORE);
-		
-		bubbleSort(rank);
+	for(i=argc;i<6;i++)
+		argv[i] = malloc(sizeof(char)*20);
+	
+	sprintf(argv[4], "%d", numFilhos);
+	sprintf(argv[5], "%d", indice);
+	
+	
+	MPI_Comm_spawn(bin, argv, numFilhos, localInfo, 0, MPI_COMM_SELF, &interComm, err);
+	
+	for(i=0;i<numFilhos;i++)
+		MPI_Send(&vetIndices[0], size*4, MPI_INT, i, 99, interComm);
+	
+	bubbleSort();	
+	
+	MPI_Finalize();
+	
+	#ifdef ELAPSEDTIME
+		gettimeofday(&end, NULL);
+		double delta = ((end.tv_sec  - start.tv_sec) * 1000000u + end.tv_usec - start.tv_usec) / 1.e6;
+		printf("Excution time\t%f\n", delta);
+	#endif
 
-		MPI_Finalize();
-
-		return 0;
+	return 0;
 }
