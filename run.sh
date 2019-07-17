e2704cd7997cec4020fd2a8dc5faf13f6e821361
Adriano Marques Garcia <adriano1mg@gmail.com>
2019-07-17 16:34:50 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -87,8 +87,20 @@ do
 done
 
 clear
-echo "Set the amount of parallel tasks to used. You can set a list separating the values by space (e.g. 2 8 16 20)."
-read -a TASKS
+
+if [[ $pthreadsIpp -eq 1 || $openmpIpp -eq 1 || $mpi1Ipp -eq 1 || $mpi2Ipp -eq 1 ]]
+then
+	echo "Set the amount of parallel tasks to used (MINIMUM 2). You can set a list separating the values by space (e.g. 2 8 16 20)."
+	read -a TASKS
+	for task in "${TASKS[@]}"
+	do
+		if [[ $task -eq 1 ]]
+        	then
+        	        printf "\n\nValue 1 for parallel tasks is not allowed.\nIt is required at least 2 tasks.\n\nStart again and choose only the sequential option for running with a single thread.\n\n"
+        	        exit 1
+        	fi
+	done
+fi
 
 clear
 echo "Set the number of executions."
@@ -97,6 +109,12 @@ read -e EXECNUMBER
 clear
 echo "Choose the size of the input set (small, medium, large, or test)."
 read -e SIZE
+re1='[Ss]mall|SMALL|[Mm]edium|MEDIUM|[Ll]arge|LARGE|[Tt]est|TEST'
+if ! [[ $SIZE =~ $re1 ]] ; then
+   echo "Invalid Size!"
+   exit 1
+fi
+
 
 printf "Selected PPIs: "
 for ipp in ${ipps[@]}
@@ -131,14 +149,14 @@ printf "\n\n"
 
 echo "Confirm? (yes/no)"
 read CONFIRM
-re2='yes|YES|y'
+re2='[Yy]es|YES|[Yy]'
 if ! [[ $CONFIRM =~ $re2 ]] ; then
    exit 1
 fi
 
 clear
 
-if [[ $SIZE = "small" ]]; then
+if [[ $SIZE = "small" || $SIZE = "Small" || $SIZE = "SMALL" ]]; then
 	#declare -a inputGL=("512")
 	declare -a inputGS=("512")
 	declare -a inputJA=("1024")
@@ -148,10 +166,10 @@ if [[ $SIZE = "small" ]]; then
 	declare -a inputDJ=("1024")
 	declare -a inputDP=("10000000000")
 	declare -a inputHA=("50000")
-	declare -a inputMM=("1024")
+	declare -a inputMM=("2048")
 	declare -a inputPI=("1000000000")
 	declare -a inputNI=("750000000")
-elif [[ $SIZE = "medium" ]]; then
+elif [[ $SIZE = "medium" || $SIZE = "Medium" || $SIZE = "MEDIUM" ]]; then
 	#declare -a inputGL=("2048")
 	declare -a inputGS=("1024")
 	declare -a inputJA=("2048")
@@ -161,10 +179,10 @@ elif [[ $SIZE = "medium" ]]; then
 	declare -a inputDJ=("2048")
 	declare -a inputDP=("15000000000")
 	declare -a inputHA=("100000")
-	declare -a inputMM=("2048")
+	declare -a inputMM=("4096")
 	declare -a inputPI=("4000000000")
 	declare -a inputNI=("1000000000")
-elif [[ $SIZE = "large" ]]; then		
+elif [[ $SIZE = "large" || $SIZE = "Large" || $SIZE = "LARGE" ]]; then		
 	#declare -a inputGL=("4096")
 	declare -a inputGS=("4096")
 	declare -a inputJA=("4096")
@@ -174,10 +192,10 @@ elif [[ $SIZE = "large" ]]; then
 	declare -a inputDJ=("4096")
 	declare -a inputDP=("20000000000")
 	declare -a inputHA=("150000")
-	declare -a inputMM=("4096")
+	declare -a inputMM=("8192")
 	declare -a inputPI=("6000000000")
 	declare -a inputNI=("1500000000")
-elif [[ $SIZE = "test" ]]; then
+elif [[ $SIZE = "test" || $SIZE = "Test" || $SIZE = "TEST" ]]; then
 	declare -a inputGL=("256")
 	declare -a inputGS=("256")
 	declare -a inputJA=("256")
@@ -223,10 +241,10 @@ for step in `seq 1 $EXECNUMBER`; do
 				input=${vseq[$n]}
 				date +"%d/%m/%Y %H:%M:%S"
 				printf "    Application: $app\n"
-				printf "    IPP: Sequential\n"
-				printf "    Input: $input\n"
+				printf "    IPP:\t Sequential\n"
+				printf "    Input:\t $input\n"
 				time=`./Apps/$app/sequential $input|grep -i time | awk {'print $3'}`
-				printf "    Time: $time\n\n"
+				printf "    Time:\t $time\n\n"
 			fi
 		done
 	fi
@@ -240,11 +258,11 @@ for step in `seq 1 $EXECNUMBER`; do
 					input=${vseq[$n]}
 					date +"%d/%m/%Y %H:%M:%S"
 					printf "    Application: $app\n"
-					printf "    IPP: Pthread\n"
-					printf "    Threads: $th\n"
-					printf "    Input: $input\n"
+					printf "    IPP:\t Pthread\n"
+					printf "    Threads:\t $th\n"
+					printf "    Input:\t $input\n"
 					time=`./Apps/$app/pthread $th $input|grep -i time | awk {'print $3'}`
-					printf "    Time: $time\n\n"
+					printf "    Time:\t $time\n\n"
 				fi
 			done
 		fi
@@ -258,11 +276,11 @@ for step in `seq 1 $EXECNUMBER`; do
 					input=${vseq[$n]}
 					date +"%d/%m/%Y %H:%M:%S"
 					printf "    Application: $app\n"
-					printf "    IPP: OpenMP\n"
-					printf "    Threads: $th\n"
-					printf "    Input: $input\n"
+					printf "    IPP:\t OpenMP\n"
+					printf "    Threads:\t $th\n"
+					printf "    Input:\t $input\n"
 					time=`./Apps/$app/openmp $th $input|grep -i time | awk {'print $3'}`
-					printf "    Time: $time\n\n"
+					printf "    Time:\t $time\n\n"
 				fi
 			done
 		fi
@@ -276,11 +294,11 @@ for step in `seq 1 $EXECNUMBER`; do
 					input=${vseq[$n]}
 					date +"%d/%m/%Y %H:%M:%S"
 					printf "    Application: $app\n"
-					printf "    IPP: MPI-1\n"
-					printf "    Threads: $th\n"
-					printf "    Input: $input\n"		
+					printf "    IPP:\t MPI-1\n"
+					printf "    Threads:\t $th\n"
+					printf "    Input:\t $input\n"		
 					time=`mpirun -np $th ./Apps/$app/mpi1 $input|grep -i time | awk {'print $3'}`
-					printf "    Time: $time\n\n"
+					printf "    Time:\t $time\n\n"
 				fi
 			done
 		fi
@@ -294,11 +312,11 @@ for step in `seq 1 $EXECNUMBER`; do
 					input=${vseq[$n]}
 					date +"%d/%m/%Y %H:%M:%S"
 					printf "    Application: $app\n"
-					printf "    IPP: MPI-2\n"
-					printf "    Threads: $th\n"
-					printf "    Input: $input\n"
+					printf "    IPP:\t MPI-2\n"
+					printf "    Threads:\t $th\n"
+					printf "    Input:\t $input\n"
 					time=`mpirun -np 1 ./Apps/$app/pai $(($th-1)) $input ./Apps/$app/filho |grep -i time | awk {'print $3'}`
-					printf "    Time: $time\n\n"
+					printf "    Time:\t $time\n\n"
 				fi
 			done
 		fi	
@@ -309,4 +327,3 @@ date +"%d/%m/%Y %H:%M:%S"
 echo "DONE!"
 
 exit
-
